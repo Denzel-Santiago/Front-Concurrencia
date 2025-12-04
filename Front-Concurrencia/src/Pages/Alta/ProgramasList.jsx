c9805ef311eb7ec724802ab886c210b8ab905730
Edwindg5 <233282@ids.upchiapas.edu.mx>
2025-12-04 03:09:24 -0600

    nada

--- Front-Concurrencia/src/Pages/Alta/ProgramasList.jsx
+++ Front-Concurrencia/src/Pages/Alta/ProgramasList.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { programasService } from "../../Services/programasService";
-
+import {programasService} from "../../Services/programasService";
 export default function ProgramasList() {
   const [programas, setProgramas] = useState([]);
   const [loading, setLoading] = useState(true);
